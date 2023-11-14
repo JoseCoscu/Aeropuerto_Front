2b0d5324a70e830c22442556afa0d3298fa8efa3
Juan José Muñoz Noda <61985033+JoseCoscu@users.noreply.github.com>
2023-11-13 19:33:47 -0500

    nuevas paginas creadas

---
+++ admin/login/script/panel.js
@@ -0,0 +1,26 @@
+const panel = document.getElementById('panel');
+const pestana = document.getElementById('pestana');
+
+pestana.addEventListener('click', () => {
+    if (panel.style.left === '-180px') {
+        panel.style.left = '0';
+    } else {
+        panel.style.left = '-180px';
+    }
+});
+
+document.getElementById('services').onclick = function(){
+    window.location.href = 'services.html';
+};
+
+document.getElementById('reparations').onclick = function(){
+    window.location.href = 'reparaciones.html';
+};
+
+document.getElementById('flys').onclick = function(){
+    window.location.href = 'vuelos.html';
+};
+
+document.getElementById('clients').onclick = function(){
+    window.location.href = 'clientes.html';
+};
