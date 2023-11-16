80f762e5b86880fb79032f8abd4a33f85e274501
Arm0101 <armando01.padron@gmail.com>
2023-11-16 15:43:33 -0500

    mostrar datos de clientes

--- admin/clients/script/fetch_data.js
+++ admin/clients/script/fetch_data.js
@@ -1,17 +1,39 @@
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZUlkIjoxLCJpYXQiOjE2OTg3NzM5MzAsImV4cCI6MTY5OTM3ODczMH0.MA_7LkLjbgYtBMOns8wuuvdv9qdCSFspsPuTL6L7shI";
 
 
-window.addEventListener('DOMContentLoaded',(event)=>{get_client()})
+window.addEventListener('DOMContentLoaded',async (event)=>{
+  const clients  = await get_client()
+  const table = document.querySelector("#clients_table tbody")
+  
+  const rows = clients.map(client => {
+    const {id, idClientType, name, username, nationality} = client 
+    return `<tr>
+              <td>${id}</td>
+              <td>${idClientType}</td>
+              <td>${name}</td>
+              <td>${username}</td>
+              <td>${nationality}</td>
+            </tr>`
+  }).join('')
+  table.innerHTML = rows;
+
+})
 
 
-const get_client = () =>{fetch("http://localhost:1234/clients", {
+const get_client = async () =>{
+
+  try {
+    const result = await fetch("http://localhost:1234/clients", {
     method: "GET",
     headers: { 
       "Authorization" : "Bearer " + encodeURIComponent(token),
       "Content-Type": "application/json"
     },
-  })
-    .then((res) => res.json())
-    .catch((error) => console.error("Error:", error))
-    .then((response) => console.log("Success:", response));
+    })
+    const clients = await result.json()
+    return clients
+  }
+  catch(error){
+    console.error(error)
+  }
 } 
