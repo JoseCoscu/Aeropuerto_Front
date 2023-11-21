1d48aa1585dec597327072f359dcc021490eb423
Arm0101 <=>
2023-11-21 09:43:04 -0500

    fixing wizard

--- admin/clients/client_form/script/wizard.js
+++ admin/clients/client_form/script/wizard.js
@@ -1,14 +1,21 @@
 // Obtener el botón y el formulario por su ID
-import { content } from "../index.js";
 const botonAbrir = document.getElementById("create_client");
 const formulario = document.getElementById("wizardForm");
-
+const getContent = async (path) => {
+  try {
+    const response = await fetch(path);
+    const content = await response.text();
+    return content;
+  } catch (e) {
+    console.error(e);
+  }
+};
 // Agregar un evento de clic al botón
 botonAbrir.addEventListener("click", async function () {
   // Mostrar u ocultar el formulario al hacer clic en el botón
   const ventanaPopup = window.open("", "", "width=700,height=500");
 
-  console.log(content);
+  const content = await getContent("./client_form/index.html");
 
   // Escribir el contenido en la nueva ventana emergente
   ventanaPopup.document.write(content);
