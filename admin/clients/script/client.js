3b48ef6dcf3fb86d940f17cbbb60900f2fa633d3
Arm0101 <=>
2023-11-23 15:52:19 -0500

    update and delete clients

--- admin/clients/script/client.js
+++ admin/clients/script/client.js
@@ -1,43 +1,47 @@
-
-var table_clients = document.querySelector("#clients_table")
+const deletClient = async (id) => {
+  const response = fetch(`http://localhost:1234/clients/${id}`, {
+    method: 'DELETE'
+  })
+  return response.ok
+}
+const table_clients = document.querySelector('#clients_table')
 table_clients.addEventListener('click', async (event) => {
+  if (event.target.type === 'submit') {
+    const button = event.target
+    if (event.target.classList.contains('edit_button')) {
+      const id_client = button.value
+      const row = Array.from(button.closest('tr').cells)
+      const client_data = {}
+      row.forEach((x) => {
+        const attr = x.getAttribute('name')
+        const value = x.textContent
+        if (attr !== null) client_data[attr] = value
+      })
+      const ventanaPopup = window.open('', 'Formulario Cliente', 'width=700,height=500')
+      const content = await getContent('./client_form/index.html')
 
-    if (event.target.type === 'submit'){
-        const button = event.target
-        if(event.target.classList.contains('edit_button')){
-            
-            const id_client = button.value
-            const row = Array.from(button.closest('tr').cells)
-            const client_data = {}
-            row.forEach(x => {
-                const attr = x.getAttribute('name')
-                const value = x.textContent
-                if (attr !== null)
-                    client_data[attr] = value
-            })
-            const ventanaPopup = window.open("", "Formulario Cliente", "width=700,height=500");
-            const content = await getContent("./client_form/index.html");
-            
-            const temporal = document.createElement('div');
-            temporal.innerHTML = content;
+      const temporal = document.createElement('div')
+      temporal.innerHTML = content
 
-            // Modificas los valores de los campos específicos
-            temporal.querySelector('#name').setAttribute('value', client_data['name']);
-            temporal.querySelector('#username').setAttribute('value', client_data['username']);
-            temporal.querySelector('#password').setAttribute('value', '1234');
-            temporal.querySelector('#nacionalidad').setAttribute('value', client_data['nationality']);
-            temporal.querySelector('#clienttype').setAttribute('value', client_data['idClientType']);
-            
-            console.log(temporal)
-            const contenidoModificado = temporal.innerHTML;
+      // Modificas los valores de los campos específicos
+      temporal.querySelector('#name').setAttribute('value', client_data['name'])
+      temporal.querySelector('#username').setAttribute('value', client_data['username'])
+      temporal.querySelector('#password').setAttribute('value', '')
+      temporal.querySelector('#nacionalidad').setAttribute('value', client_data['nationality'])
+      temporal.querySelector('#clienttype').setAttribute('value', client_data['idClientType'])
 
-            // Escribir el contenido en la nueva ventana emergente
-             ventanaPopup.document.write( contenidoModificado);
+      temporal.querySelector('#create-update').setAttribute('value', 'Edit')
 
-        }
-        if(event.target.classList.contains('delete_button')){
-            const id_client = button.value
-            console.log(id_client)
-        }
+      temporal.querySelector('#idClient').setAttribute('value', id_client)
+      const contenidoModificado = temporal.innerHTML
+
+      // Escribir el contenido en la nueva ventana emergente
+      ventanaPopup.document.write(contenidoModificado)
     }
+    if (event.target.classList.contains('delete_button')) {
+      const id_client = button.value
+      const response = await deletClient(id_client)
+      console.log(response)
+    }
+  }
 })
--- admin/clients/script/fetch_data.js
+++ admin/clients/script/fetch_data.js
@@ -1,13 +1,15 @@
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZUlkIjoxLCJpYXQiOjE2OTg3NzM5MzAsImV4cCI6MTY5OTM3ODczMH0.MA_7LkLjbgYtBMOns8wuuvdv9qdCSFspsPuTL6L7shI";
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZUlkIjoxLCJpYXQiOjE2OTg3NzM5MzAsImV4cCI6MTY5OTM3ODczMH0.MA_7LkLjbgYtBMOns8wuuvdv9qdCSFspsPuTL6L7shI'
+const table = document.querySelector('#clients_table tbody')
+const refresh_button = document.querySelector('#refresh')
 
+const refreshClients = async (event) => {
+  const clients = await get_client()
 
-window.addEventListener('DOMContentLoaded',async (event)=>{
-  const clients  = await get_client()
-  const table = document.querySelector("#clients_table tbody")
-  
-  const rows = clients.map(client => {
-    const {id, idClientType, name, username, nationality} = client 
-    return `<tr>
+  const rows = clients
+    .map((client) => {
+      const { id, idClientType, name, username, nationality } = client
+      return `<tr>
               <td name = 'id'>${id}</td>
               <td name = 'idClientType'>${idClientType}</td>
               <td name = 'name'>${name}</td>
@@ -18,26 +20,26 @@ window.addEventListener('DOMContentLoaded',async (event)=>{
               <button class = "boton delete_button" value =${id}>Delete</button>
               </td>
             </tr>`
-  }).join('')
-  table.innerHTML = rows;
+    })
+    .join('')
+  table.innerHTML = rows
+}
 
-})
+window.addEventListener('DOMContentLoaded', refreshClients)
+refresh_button.addEventListener('click', refreshClients)
 
-
-const get_client = async () =>{
-
+const get_client = async () => {
   try {
-    const result = await fetch("http://localhost:1234/clients", {
-    method: "GET",
-    headers: { 
-      "Authorization" : "Bearer " + encodeURIComponent(token),
-      "Content-Type": "application/json"
-    },
+    const result = await fetch('http://localhost:1234/clients', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + encodeURIComponent(token),
+        'Content-Type': 'application/json'
+      }
     })
     const clients = await result.json()
     return clients
-  }
-  catch(error){
+  } catch (error) {
     console.error(error)
   }
-} 
+}
